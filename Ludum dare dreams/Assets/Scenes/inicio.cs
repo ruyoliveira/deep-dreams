c21343084640dc6d7b03764c4b4526d6c45d088b
Ruy "Lorde Supremo " Oliveira <ruygsgoliveira@live.com>
2021-05-08 14:44:49 -0300

    Update inicio.cs

--- Ludum dare dreams/Assets/Scenes/inicio.cs
+++ Ludum dare dreams/Assets/Scenes/inicio.cs
@@ -7,7 +7,7 @@ public class inicio : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        Debug.Log("Inicio");
     }
 
     // Update is called once per frame
